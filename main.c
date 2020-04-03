266238b3e06888a4f097bd1a4991898bd1fbd751
Mateusz <mateusz.marat@gmail.com>
2020-04-03 17:55:04 +0200

    mój pierwszy commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Mateusz Marat\n");
+    printf("Mateusz\nMarat\n");
+    printf("Mateusz ");
+    printf("Marat\n");
+    return 0;
+}
